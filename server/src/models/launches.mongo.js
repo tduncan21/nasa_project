d794f8c3fff80d7408ab317ec7d7c8bbfdd0ab77
tduncan94 <83376555+tduncan94@users.noreply.github.com>
2022-07-08 10:39:52 -0500

    Add db connection

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const launchesSchema = new mongoose.Schema({
+    flightNumber: {
+        type: Number,
+        required: true,
+    },
+    mission: {
+        type: String,
+        required: true,
+    },
+    rocket: {
+        type: String,
+        required: true,
+    },
+    launchDate: {
+        type: Date,
+        required: true,
+    },
+    target: {
+        type: String,
+        required: true,
+    },
+    customers: [ String ],
+    upcoming: {
+        type: Number,
+        required: true,
+    },
+    success: {
+        type: Number,
+        required: true,
+        default: true,
+    },
+})
